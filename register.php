56a10a180a6cb8b72e8f7df0d931c8af101903e4
arthur <arthurtheisen05@outlook.com>
2023-12-05 16:54:08 -0300

    email e senha validation

--- register.php
+++ register.php
@@ -1,12 +1,41 @@
 <?php
 
-$conexao = new mysqli("localhost","root","","godfield");
+$conexao = new mysqli("localhost", "root", "", "godfield");
 
-$sql = "INSERT INTO usuario (nome,email,senha) 
-VALUES ('{$_POST['nome']}','{$_POST['email']}','{$_POST['senha']}')";
 
-$conexao->query($sql);
+//validação do email
+$email = mysqli_real_escape_string($conexao, $_POST['email']);
+if (empty($email)) {
+    
+    echo "<script>alert('Email é obrigatório.'); window.location.href='formRegister.php';</script>";
+    exit;
+} else if (!filter_var($email, FILTER_VALIDATE_EMAIL) || strpos($email, "@aluno.feliz.ifrs.edu.br") === false) {
+    echo "<script>alert('Email inválido. Apenas emails @aluno.feliz.ifrs.edu.br são aceitos.'); window.location.href='formRegister.php';</script>";
+    exit;
+}
+// validacao da senha
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+if (empty($senha)) {
+    echo "<script>alert('Senha é obrigatória.'); window.location.href='formRegister.php';</script>";
+    exit;
+} else if (strlen($senha) < 5) {
+    echo "<script>alert('Senha deve ter pelo menos 5 caracteres.'); window.location.href='formRegister.php';</script>";
+    exit;
+}
 
-header("location: formLogin.php");
+$nome = mysqli_real_escape_string($conexao, $_POST['nome']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+$sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+
+if ($conexao->query($sql)) {
+=
+    header("location: formLogin.php");
+} else {
+
+    echo "Erro ao registrar usuário: " . $conexao->error;
+}
+
+$conexao->close();
 
 ?>
